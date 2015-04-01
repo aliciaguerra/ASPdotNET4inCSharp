0f570128abd11538a53092d1e1d96367ba35e2f2
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-31 23:51:14 -0700

    Create path.cs

---
+++ path.cs
@@ -0,0 +1,5 @@
+//A C# variable holding the 
+//c:\MyApp\MyFiles path.
+path="c:\\MyApp\\MyFiles";
+//Alternatively, you can turn off C# by preceding a string with an @ symbol as shown
+path=@"c:\MyApp\Files";
