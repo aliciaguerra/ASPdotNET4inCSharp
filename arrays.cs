ecaa58a143e02c610ca778bdef5ae9a2696ae6ee
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-01 00:00:41 -0700

    Create arrays.cs

---
+++ arrays.cs
@@ -0,0 +1,20 @@
+//Create an array with four strings (from index 0 to index 3).
+//You need to initialize the array with the
+//new keyword in order to use it.
+string[] stringArray = new String[4];
+
+//Create a 2x4 grid array (with a total of eight integers).
+int[,] intArray = new int[2,4];
+
+//create an array with four strings, one for each number from 1 to 4
+string[] stringArray = {"1", "2", "3", "4"};
+
+//Create a 4x2 array (a grid with four rows and two columns).
+int[,] intArray = {{1,2}. {3,4}, {5,6}, {7,8}};
+
+int[] intArray={1,2,3,4};
+int element=stringArray[2]; //element is now set to 3
+
+int[,] intArray = {{1,2}, {3,4}, {5,6}, {7,8}};
+//Access the value in the row 0 (first row), column 1(second column).
+int element=intArray[0,1]; //element is now set to 2
