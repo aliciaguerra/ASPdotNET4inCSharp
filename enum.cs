d1ed2275bcdb492e7fa48144638b2516e84882e5
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-01 00:12:00 -0700

    Create enum.cs

---
+++ enum.cs
@@ -0,0 +1,10 @@
+//Define an enumeration type named UserType with three possible values.
+enum UserType
+{
+Admin;
+Guest;
+Invalid;
+}
+
+//Create a new value and set it equal to the UserType.Admin constant.
+UserType newUserType = UserType.Admin;
